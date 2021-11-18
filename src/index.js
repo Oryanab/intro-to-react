14391a80bc1ccea167385b61b31615297a32d7eb
Oryanab <86202122+Oryanab@users.noreply.github.com>
2021-11-18 19:29:24 +0200

    update 3.0 -final result

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -102,6 +102,7 @@ class Game extends React.Component {
           squares: squares,
         },
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
@@ -113,7 +114,7 @@ class Game extends React.Component {
   }
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move ? "Go to move #" + move : "Go to game start";
